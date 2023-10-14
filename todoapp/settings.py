afab2986e2afc3b4a2aeff360989b360aaa5869e
SurajSuresh123 <surajsuresh016@gmail.com>
2023-10-14 19:13:03 +0530

    todoapp commit

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'todolist',
+        'USER':'test_user',
+        'PASSWORD':'password',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
